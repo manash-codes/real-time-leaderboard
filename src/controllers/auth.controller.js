51f8a9defdb74ab5a62bb018427f2d865b53195a
manash-codes <manash.codes@gmail.com>
2026-04-05 15:41:27 +0530

    Add Password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,12 @@
-const register = (req, res) => {
-    res.json({ title: "called" })
+const User = require('../models/User.model');
+
+const register = async (req, res) => {
+    const { name, email, username, password } = req.body;
+
+    const user = new User({ name, email, username, password });
+    await user.save();
+
+    res.json({ success: true, message: "User added successfully", user })
 }
 
 const login = (req, res) => {
---
+++ src/models/User.model.js
@@ -0,0 +1,24 @@
+var { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model('User', userSchema);
