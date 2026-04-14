7eab6f3bc5fb02a96a2d69f4747c4b4a8a16f042
manash-codes <manash.codes@gmail.com>
2026-04-14 22:29:27 +0530

    Update User model for removing __v & password

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -4,21 +4,38 @@ const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
     },
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
-});
+},
+    {
+        timestamps: true,
+        toJson: {
+            transform(doc, ret) {
+                delete ret.password
+                delete ret.__v
+                return ret
+            }
+        }
+    });
 
 module.exports = model('User', userSchema);
