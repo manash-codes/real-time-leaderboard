8e65e9225552740db89f1833f608f57e2dcd85a3
manash-codes <manash.codes@gmail.com>
2026-04-15 17:56:14 +0530

    Update auth middleware to remove password from get

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,14 +2,14 @@ const UserModel = require("../models/User.model");
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
     if (!token) return res.status(401).json({ success: false, message: "No token provided" });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if (!decoded) return res.status(401).json({ success: false, message: "Invalid token" });
 
-        const user = await UserModel.findById(decoded.id);
+        const user = await UserModel.findById(decoded.id).select('-password');
         if (!user) return res.status(401).json({ success: false, message: "User not found" });
         req.user = user;
         next();
